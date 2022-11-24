deb371f69cbffb5aa07bf8364658acdab46ac420
marcelo-antonio-nunes <marcelo197519@gmail.com>
2022-11-24 08:30:12 -0300

    Primeiro refadoramento das func

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from sqlitehelper.database import DatabaseHelper as db
 
 app = Flask(__name__)
 
+
+# ---------------------------------------------- lista todos as gavetas
 @app.route('/lista')
 def lista():
     dt = db()
@@ -13,41 +15,54 @@ def lista():
     itens=len(componentes)
     return render_template('lista.html',lista=componentes,len=itens)
 
-@app.route('/cadastrar', methods=['POST','GET'])
+# ---------------------------------------------- cria nova gaveta
+@app.route('/cadastrar', methods=['GET',])
 def cadastrar():
+    return render_template('cadastrar.html')
+
+@app.route('/novo', methods=['POST',])
+def novo():
     dt = db()
-    if request.method == "POST":
-        componente = Componente(
-            request.form["gaveta"],
-            request.form["tipo"],
-            request.form["codigo"],
-            request.form["quantidade"])
-        dt.cria_tabela()
-        dt.cadastar_gaveta(componente)
-        redirect("/lista")
-    return render_template('cadastrar.html',)
+    componente = Componente(
+        request.form["gaveta"],
+        request.form["tipo"],
+        request.form["codigo"],
+        request.form["quantidade"])
+    dt.cria_tabela()
+    dt.cadastar_gaveta(componente)
+    return redirect('/lista')
+
+#----------------------------------------- fim
 
 
-@app.route('/saida', methods=['POST','GET'])
+
+# --------------------------------------- adicionar componente
+@app.route('/entrada', methods=['GET',])
+def entrada():
+    return render_template("entrada.html")
+
+@app.route('/adicionar',methods=['POST',])
+def adicionar():
+    dt = db()
+    g = request.form["gaveta"]
+    c = request.form["quantidade"]
+    dt.adicionar_componentes(g, c)
+    return redirect('/lista')
+# --------------------------------------- fim
+
+# --------------------------------------- retirar componente
+@app.route('/saida', methods=['GET',])
 def saida():
-    dt = db()
-    if request.method == "POST":
-        g = request.form["gaveta"]
-        c = request.form["quantidade"]
-        dt.retirar_componentes(g, c)
-        redirect("/lista")
     return render_template("saida.html")
 
-@app.route('/entrada', methods=['POST','GET'])
-def entrada():
-    if request.method == "POST":
-        dt = db()
-        g = request.form["gaveta"]
-        c = request.form["quantidade"]
-        dt.adicionar_componentes(g, c)
-        return render_template("lista.html")
-    else:
-        return render_template("entrada.html")
+@app.route('/retirar', methods=['POST',])
+def retirar():
+    dt = db()
+    g = request.form["gaveta"]
+    c = request.form["quantidade"]
+    dt.retirar_componentes(g, c)
+    return redirect("/lista")
+# ------------------------------------fim
 
 
 
